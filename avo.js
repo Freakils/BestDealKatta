fdede747522e099c9961f5e94afa4c5ae111d61f
Freakils <luvo@projectcodex.co>
2016-03-08 13:13:33 +0200

    Avocados

---
+++ avo.js
@@ -0,0 +1,23 @@
+// function hello(){
+// 		return "Hello World";
+// };
+
+// var avoString = [];
+
+function findAvoDeals(avoString){
+  	var avoDeals = avoString.split(",")
+  	return avoDeals;
+	};
+
+
+// var avoString = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+// 	var avoDeals
+// function findAvoDeals(avoString){
+			 
+// 			var avoDealsList = [];
+//     		for(var i=0;i<avos.length;i++){
+//         	var avoDeals = avoDealsList[i];
+
+//         	var avoDeals = avoString
+//             
+// };
---
+++ avo_test.js
@@ -0,0 +1,31 @@
+// QUnit.test( "testing hello function", function( assert ) {
+// 		var result = hello();
+// 			assert.equal( "Hello World", result, "testing hello function");
+// });
+
+QUnit.test( "findAvoDeals", function( assert ) {
+		var result = findAvoDeals("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+
+			assert.equal(["1 for R3", " 2 for R7", " 3 for R10", " 5 for R14.50."], result);
+});
+
+// QUnit.test( "find the cheapest deal", function( assert ) {
+
+//     var Avos = findAvos("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+
+//     assert.equal( "", Avos);
+// });
+
+// QUnit.test( "find the most expensive deal", function( assert ) {
+
+//     var Avos = findAvos("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+
+//     assert.equal( "", Avos);
+// });
+
+// QUnit.test( "find the average price across all the deals", function( assert ) {
+
+//     var Avos = findAvos("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+
+//     assert.equal( "", Avos);
+// });
