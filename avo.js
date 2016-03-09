15b301925ee251614e20c0a2b741fdda26c1ed06
Freakils <luvo@projectcodex.co>
2016-03-09 17:44:14 +0200

    separetion

--- avo.js
+++ avo.js
@@ -3,19 +3,34 @@ function hello(){
 };
 
 function findAvoDeals(avoString){
-  	var avoDeals = avoString.split(",")
-  	return avoDeals;
-	};
-
-
-// var avoString = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
-// 	var avoDeals
-// function findAvoDeals(avoString){
+  	var avoDeals = avoString.split(",");
+  	// return avoDeals;
 			 
-// 			var avoDealsList = [];
-//     		for(var i=0;i<avos.length;i++){
-//         	var avoDeals = avoDealsList[i];
+	var avoDealsList = [];
+    		for(var i=0; i<avoDeals.length; i++){
+        	var avoDealsString = avoDeals[i];
+        	
+        	    var fish = avoDealsString
+        	    	.split("for");
+				
+				var quantity = Number(fish[0]);
+                  
 
-//         	var avoDeals = avoString
-//             
-// };
+                var pricenumber = fish[1]
+                	.replace("R","");
+
+                var prices = Number(pricenumber);
+                    avoDealsList.push({
+						quantity:quantity,
+                        prices:prices,
+					});
+				
+        		        	    
+
+
+	}
+	return avoDealsList;
+	// console.log(avoDealsList);
+};
+
+console.log(findAvoDeals("1 for R3, 2 for R7, 3 for R10, 5 for R14.50"));
--- avo_test.js
+++ avo_test.js
@@ -3,10 +3,27 @@ QUnit.test( "testing hello function", function( assert ) {
 			assert.equal( "Hello World", result, "testing hello function");
 });
 
-QUnit.test( "findAvoDeals", function( assert ) {
-		var result = findAvoDeals("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+QUnit.test("findAvoDeals convert the string into an array (split out the quantity and the price", function( assert ) {
+		var result = findAvoDeals("1 for R3, 2 for R7, 3 for R10, 5 for R14.50");
+		var expectedList = [{
+							  prices: 3,
+							  quantity: 1
+							},
+							{
+							  prices: 7,
+							  quantity: 2
+							},
+							{
+							  prices: 10,
+							  quantity: 3
+							},
+							{
+							  prices: 14.5,
+							  quantity: 5
+							}];
 
-			assert.deepEqual(["1 for R3", " 2 for R7", " 3 for R10", " 5 for R14.50."], result);
+			assert.deepEqual(expectedList, result);
+
 });
 
 // QUnit.test( "find the cheapest deal", function( assert ) {
